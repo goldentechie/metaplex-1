5973a9f2b5f3a76ce875ae63d94f907206b6bb8b
goldentechie <goldentechie3@gmail.com>
2021-11-05 16:28:45 -0400

    Punch list - mint, auction, wallet feature finished

--- js/packages/web/src/components/AppBar/index.tsx
+++ js/packages/web/src/components/AppBar/index.tsx
@@ -50,7 +50,7 @@ const MetaplexMenu = () => {
     return (
       <>
         <Modal
-          title={<img src={'/metaplex-logo.svg'} />}
+          title={<img className="logo" src={'/meta-mint.png'} />}
           visible={isModalVisible}
           footer={null}
           className={'modal-box'}
@@ -109,7 +109,7 @@ const MetaplexMenu = () => {
 export const LogoLink = () => {
   return (
     <Link to={`/`}>
-      <img src={'/metaplex-logo.svg'} />
+      <img className="logo" src={'/meta-mint.png'} />
     </Link>
   );
 };
